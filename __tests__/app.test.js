68209aa77669c689cc02a0c22f7937b196def262
Fabio Giannico <fabiogiannico3@gmail.com>
2025-08-26 16:25:37 +0200

    Implemented newExampleGreeting test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,9 @@
-const greet = require('../app');
+const { greet, newExampleGreeting } = require('../app');
 
 test('greets the user by name', () => {
   expect(greet('DevOps')).toBe('Hello, DevOps!');
+});
+
+test('newExampleGreeting returns the correct message', () => {
+  expect(newExampleGreeting()).toBe('This function was created by Fabio G.');
 });
